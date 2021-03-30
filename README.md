b2ccd1f392672f26e4dc0bb2c1587db43a505cab
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-03-30 16:31:45 +0200

    Algorand local network testing

--- README.md
+++ README.md
@@ -9,12 +9,40 @@ To run examples:
 
 ## Algorand
 
-Obtain an API key at [Pure Stake](https://www.purestake.com/technology/algorand-api/) and put it as `ALGORAND_API_KEY` in `.env` file.
+### Pure Stake provider
+
+Obtain an API key at [Pure Stake](https://www.purestake.com/technology/algorand-api/) and provide configuration in `.env` file:
+
+```
+ALGO_PURESTAKE_API=true
+
+ALGO_API_KEY=<your-api-key>
+ALGO_ALGODV2="https://testnet-algorand.api.purestake.io/ps2"
+ALGO_ALGODV2_PORT=""
+ALGO_IDXV2="https://testnet-algorand.api.purestake.io/idx2"
+ALGO_IDXV2_PORT=""
+```
+
+### Local private network
+
+Pure Stake doesn't offer API for participation in consensus - this doesn't matter at all currently, since offline accounts gather participation rewards at the same rate as online accounts (the only condition is balance at least 1 ALGO).
+
+Follow the [sandbox](https://github.com/algorand/sandbox) instructions to spin up a local private network. Provide configuration in `.env` file:
+
+```
+ALGO_PURESTAKE_API=false
+
+ALGO_API_KEY="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+ALGO_ALGODV2="http://localhost"
+ALGO_ALGODV2_PORT="4001"
+ALGO_IDXV2="http://localhost"
+ALGO_IDXV2_PORT="8980"
+```
 
 ## Bitcoin
 
 We use [Blockbook](https://github.com/trezor/blockbook/) as indexer and [bitcoinjs-lib](https://github.com/bitcoinjs/bitcoinjs-lib) for transaction building.
 
-### Cosmos
+## Cosmos
 
-The landscape is extremely volatile. Current latest testnet: https://stargate.cosmos.network/testnet. No faucet seems available at this moment. Follow this invite to [Cosmos discord](https://discord.gg/HbvhWZw), join the Stargate testnet channel and politely ask for funds there. Keep an eye on [CosmJS](https://github.com/cosmos/cosmjs) library developments and documentation updates.
+The landscape is extremely volatile. Current latest testnet: https://stargate.cosmos.network/testnet. No faucet seems available at this moment. Follow this invite to [Cosmos discord](https://discord.gg/HbvhWZw), join the Stargate testnet channel and politely ask for funds there. Keep an eye on [CosmJS](https://github.com/cosmos/cosmjs) library for developments and documentation updates.
--- algorand.js
+++ algorand.js
@@ -3,12 +3,12 @@ require("dotenv").config()
 
 
 // https://developer.algorand.org/tutorials/creating-javascript-transaction-purestake-api/
-const PURESTAKE_API = true
-const token = PURESTAKE_API ? { "X-API-Key": process.env.ALGORAND_API_KEY } : process.env.ALGORAND_API_KEY
+const purestake = process.env.ALGO_PURESTAKE_API.toLowerCase() === "true"
+const token = purestake ? { "X-API-Key": process.env.ALGO_API_KEY } : process.env.ALGO_API_KEY
 
-const client = new algosdk.Algodv2(token, "https://testnet-algorand.api.purestake.io/ps2", "")
-const indexer =  new algosdk.Indexer(token, "https://testnet-algorand.api.purestake.io/idx2", "")
-// const kmd = new algosdk.Kmd(API_KEY, "http://localhost") // not available on purestake node
+const client = new algosdk.Algodv2(token, process.env.ALGO_ALGODV2, process.env.ALGO_ALGODV2_PORT)
+const indexer =  new algosdk.Indexer(token, process.env.ALGO_IDXV2, process.env.ALGO_IDXV2_PORT)
+// const kmd = new algosdk.Kmd(token, "http://localhost", "4002")  // not available on purestake node
 const mnemonic1 = "cheap elevator resource claw gas census giant hero beauty series brown will impulse canyon husband stuff panda viable iron inch when screen rapid able law"
 const mnemonic2 = "abandon arrow round rebel glory truly jungle tide find napkin left rough expose vivid shallow few need switch purchase snack hazard swing car able catch"
 
@@ -21,15 +21,51 @@ async function waitForConfirmation(txid) {
             console.log(`Transaction confirmed in round ${pendingInfo["confirmed-round"]}`)
             break
         }
-        lastround++;
+        lastround++
         await client.statusAfterBlock(lastround).do()
-     }
- }
+    }
+}
+
+
+async function fundTransferTransaction(account1, account2, amount) {
+    // construct transaction
+    let params = await client.getTransactionParams().do()
+    console.log("Suggested tx params:", params)
+    let note = algosdk.encodeObj("From Vacublus to Vacublus")
+    let tx = algosdk.makePaymentTxnWithSuggestedParams(account1.addr, account2.addr, amount, undefined, note, params)
+    // console.log("Prepared transaction:", tx)
+
+    // sign transaction
+    const signedTx = algosdk.signTransaction(tx, account1.sk)
+
+    // send and wait for confirmation
+    const sendTx = await client.sendRawTransaction(signedTx.blob).do()
+    console.log("Sent transaction id:", sendTx.txId, "waiting for confirmation...")
+    await waitForConfirmation(sendTx.txId)
+    console.log("Account 1:", account1.addr, (await client.accountInformation(account1.addr).do()).amount)
+    console.log("Account 2:", account2.addr, (await client.accountInformation(account2.addr).do()).amount)
+}
+
+
+async function participationTransaction(account1) {
+    // construct transaction
+    let params = await client.getTransactionParams().do()
+    console.log("Suggested tx params:", params)
+    let tx = algosdk.makeKeyRegistrationTxnWithSuggestedParams(account1.addr, account2.addr, amount, undefined, note, params)
+
+    // sign transaction
+    const signedTx = algosdk.signTransaction(tx, account1.sk)
+
+    // send and wait for confirmation
+    const sendTx = await client.sendRawTransaction(signedTx.blob).do()
+    console.log("Sent transaction id:", sendTx.txId, "waiting for confirmation...")
+    await waitForConfirmation(sendTx.txId)
+}
 
 
 async function main() {
     // new account
-    // console.log(algosdk.secretKeyToMnemonic(algosdk.generateAccount().sk))
+    console.log(algosdk.secretKeyToMnemonic(algosdk.generateAccount().sk))
 
     // console.log("Client status:", await client.status().do())
     // console.log("Some block info:", await indexer.lookupBlock(100).do())
@@ -41,27 +77,24 @@ async function main() {
     let info2 = await client.accountInformation(account2.addr).do()
     console.log("Account 1:", account1.addr, info1.amount)
     console.log("Account 2:", account2.addr, info2.amount)
-    if (info1.amount < 10000)
-        console.log(`Please fund ${account1.addr} from faucet https://bank.testnet.algorand.network/`)
+    if (info1.amount < 10000) {
+        let fundMsg = purestake ? "from faucet https://bank.testnet.algorand.network/"
+                                : "using goal: \n ./sandbox goal account list"
+                                  + `\n ./sandbox goal clerk send -a 10000000 -f <from> -t ${account1.addr}`
+        console.log(`Please fund ${account1.addr} ${fundMsg}`)
+        return
+    }
     
-    // construct transaction
-    let params = await client.getTransactionParams().do()
-    console.log("Suggested tx params:", params)
-    let note = algosdk.encodeObj("From Vacublus to Vacublus")
-    let tx = algosdk.makePaymentTxnWithSuggestedParams(account1.addr, account2.addr, 100000, undefined, note, params)
-    // console.log("Prepared transaction:", tx)
+    // fund transfer example
+    await fundTransferTransaction(account1, account2, 100000)
 
-    // sign transaction
-    const signedTx = algosdk.signTransaction(tx, account1.sk)
-
-    // send and wait for confirmation
-    const sendTx = await client.sendRawTransaction(signedTx.blob).do()
-    console.log("Sent transaction id:", sendTx.txId);
-    await waitForConfirmation(sendTx.txId)
-    console.log("Account 1:", account1.addr, (await client.accountInformation(account1.addr).do()).amount)
-    console.log("Account 2:", account2.addr, (await client.accountInformation(account2.addr).do()).amount)
-
-    // add participation demo
+    // consensus participation
+    // TODO: only do this once there are any upsides to being online in consensus
+    // (https://algorand.foundation/faq#participation-rewards-)
+    // Step 1.: generating partkey has to be done through goal - no endpoint currently
+    // (https://github.com/algorand/go-algorand/issues/864)
+    // Step 2.: algosdk.makeKeyRegistrationTxnWithSuggestedParams
+    console.log(await indexer.lookupAccountByID(account1.addr).do())
 }
 
 
