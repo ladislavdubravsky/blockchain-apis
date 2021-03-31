5256db01fcc2b5460c8793d44cabac5a25e261d6
Ladislav Dubravsky <dubravsky90@gmail.com>
2021-03-31 18:00:24 +0200

    Delegation example

--- cosmos.js
+++ cosmos.js
@@ -1,6 +1,7 @@
 const ps = require("@cosmjs/proto-signing")
 const sg = require("@cosmjs/stargate")
 const launchpad = require("@cosmjs/launchpad")
+const txStake = require("@cosmjs/stargate/build/codec/cosmos/staking/v1beta1/tx")
 
 
 // https://stargate.cosmos.network/testnet
@@ -22,13 +23,13 @@ async function main() {
     const client2 = await sg.SigningStargateClient.connectWithSigner(rpcEndpoint, wallet2, options)
     const [account1] = await wallet1.getAccounts()
     const [account2] = await wallet2.getAccounts()
-    console.log("Current height:", await client1.getHeight())
+    // console.log("Current height:", await client1.getHeight())
     // console.log("Original funding tx:", await client1.getTx("05F6B3C1F299C8CE5033D9B41A3F37823889AE8C01C5436C2B23F4B12568EDA3"))
     console.log("Account 1:", account1.address, await client1.getBalance(account1.address, "umuon"))
     console.log("Account 2:", account2.address, await client2.getBalance(account2.address, "umuon"))
 
     // fund transfer
-    const sendMsg = {
+    let msg = {
         typeUrl: "/cosmos.bank.v1beta1.MsgSend",
         value: {
             fromAddress: account1.address,
@@ -36,10 +37,30 @@ async function main() {
             amount: [{ denom: "umuon", amount: "10000" }]
         }
     }
-    let result = await client1.signAndBroadcast(account1.address, [sendMsg], client1.fees.send, "Memo")
+    let result = await client1.signAndBroadcast(account1.address, [msg], client1.fees.send, "Memo")
     sg.assertIsBroadcastTxSuccess(result)
     console.log("Account 1:", account1.address, await client1.getBalance(account1.address, "umuon"))
     console.log("Account 2:", account2.address, await client2.getBalance(account2.address, "umuon"))
+
+    // if we wanted to become a validator - big responsibility on security, uptime, top 125 stake
+    // https://hub.cosmos.network/main/validators/validator-setup.html
+
+    // delegate a bit of stake to a validator
+    let msg = {
+        typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
+        value: txStake.MsgDelegate.fromJSON({
+            delegatorAddress: account1.address,
+            validatorAddress: "cosmosvaloper105gvcjgs6s4j5ws9srckx0drt4x8cwgywplh7p",
+            amount: { denom: "umuon", amount: "100000" },
+        }),
+    }
+    let fee = {
+        amount: [{ denom: "umuon", amount: "4000" }],
+        gas: "180000",
+    }
+    let result = await client1.signAndBroadcast(account1.address, [msg], fee, "Memo")
+    sg.assertIsBroadcastTxSuccess(result)
+    console.log("Stake delegation successfull. See txid:", result)
 }
 
 
